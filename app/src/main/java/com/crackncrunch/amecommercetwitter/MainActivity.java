281868d18dc69c2f17f0870a6f4c33d5b35d0f66
Lilian Burdianov <lilian.burdianov@gmail.com>
2017-04-04 21:41:41 +0400

    Implement Twitter boilerplate code

--- app/src/main/java/com/crackncrunch/amecommercetwitter/MainActivity.java
+++ app/src/main/java/com/crackncrunch/amecommercetwitter/MainActivity.java
@@ -1,13 +1,25 @@
 package com.crackncrunch.amecommercetwitter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+    private static final String TWITTER_KEY = "bMOpef36pEVqzvzk3FrcEszC4";
+    private static final String TWITTER_SECRET = "0fom4F5B5CIH6l5gJrmDjJWkLDrURREA0bH5iiuaDhzByf2Ntg";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new Twitter(authConfig));
         setContentView(R.layout.activity_main);
     }
 }
